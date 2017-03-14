c7a958a6ee0aac48d77a1c5d8db509762bc703a9
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-15 01:28:49 +0200

    Create Message model

--- wallapp/api/models.py
+++ wallapp/api/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.body
--- wallapp/api/tests.py
+++ wallapp/api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from api.models import Message
 
-# Create your tests here.
+class MessageModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='someuser', first_name='some', last_name='user', email='s@example.com', password='simplepassword')
+        self.message = Message(author=self.user, body='message body')
+
+    def test_create_message(self):
+        old_count = Message.objects.count()
+        self.message.save()
+        new_count = Message.objects.count()
+        self.assertEqual(new_count-old_count, 1)
+        self.assertNotEqual(new_count, old_count)
+
+    def test_str_representation(self):
+        self.assertEqual(unicode(self.message), self.message.body)
