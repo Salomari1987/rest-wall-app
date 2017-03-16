6ed4efb2c3ae1710485fd45fb93f554ba76396d0
Salah Alomari <s.z.alomari.1987@gmail.com>
2017-03-16 15:55:08 +0200

    Add token authentication

--- wallapp/api/models.py
+++ wallapp/api/models.py
@@ -1,5 +1,10 @@
 from __future__ import unicode_literals
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+
 
 class Message(models.Model):
     author = models.ForeignKey('auth.User', related_name='messages', on_delete=models.CASCADE)
@@ -9,3 +14,10 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body
+
+
+# This receiver handles token creation immediately a new user is created.
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
--- wallapp/api/urls.py
+++ wallapp/api/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from api.views import MessageView
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = {
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')), 
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^messages/$', MessageView.as_view(), name="messages"),
+    url(r'^get-token/', obtain_auth_token),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
